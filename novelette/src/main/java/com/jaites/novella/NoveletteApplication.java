782c503b27de91e154916593c65a7ac12087378a
Jaites <jupadim.ac@gmail.com>
2020-05-13 20:41:26 +0100

    Initial Commit.

---
+++ novelette/src/main/java/com/jaites/novella/NoveletteApplication.java
@@ -0,0 +1,13 @@
+package com.jaites.novella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoveletteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoveletteApplication.class, args);
+	}
+
+}
---
+++ novelette/src/test/java/com/jaites/novella/NoveletteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaites.novella;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoveletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
